f4e433e799aad8b371335985c9ebfd54800cf40b
Buddy1875 <buddy1875@hotmail.com>
2020-11-18 18:44:25 +0700

    add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
+
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
+import { AngularFireModule } from "@angular/fire";
+import { enviroment } from "./enviroment";
+
 
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
---
+++ src/app/enviroment.ts
@@ -0,0 +1,12 @@
+export const enviroment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyB-HhPG14IFGXmPbRQ4j-4_v8AWP9M8984",
+    authDomain: "dobbys-sock-35403.firebaseapp.com",
+    databaseURL: "https://dobbys-sock-35403.firebaseio.com",
+    projectId: "dobbys-sock-35403",
+    storageBucket: "dobbys-sock-35403.appspot.com",
+    messagingSenderId: "205025192302",
+    appId: "1:205025192302:web:7d15e49d5a6950a172797f",
+    measurementId: "G-JJ8V3PP943"
+  }
+};
