ee39c80cf3e89fff5bafe440c7de2167ee5ca7ad
Buddy1875 <buddy1875@hotmail.com>
2020-11-18 13:46:20 +0700

    add container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HomeComponent } from './home/home.component';
+import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HomeComponent ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, MovieComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
